916becf785480b16a289cd38f1b40479f1bf5933
Cassy <119490174+cassy25@users.noreply.github.com>
2024-01-31 17:10:55 +0100

    Create main.cpp

---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgeoffra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 14:12:25 by cgeoffra          #+#    #+#             */
+/*   Updated: 2023/11/29 14:12:27 by cgeoffra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
